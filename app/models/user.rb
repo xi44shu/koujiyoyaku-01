04643acf1f0bf4a652515d64f32ab7e2e5e3500a
joni-deppu <44shuxichuan@gmail.com>
2022-09-06 18:06:48 +0900

    rspec導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
   validates :first_name, presence: true
+  end
        
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
